8bdec964cddff055be0ab1b05ec86a34e5531167
Vaishnavi Gajinkar <vaishnavi.gajinkar@gmail.com>
2025-02-26 23:58:16 +0530

    files & advance funcs 26 Feb

--- userDefinedfunctions/Hard/middleChar.py
+++ userDefinedfunctions/Hard/middleChar.py
@@ -1,2 +1,28 @@
 '''print the middle character of a string (both odd/even length)'''
 
+file = open(r'C:\Users\Lenovo\OneDrive\Documents\PythonPractise\CodingWise\userDefinedfunctions\Hard\fibbo.txt','r')
+
+words = file.read()
+# print(type(words))
+cleaned_str = words.replace(".","")
+cleaned_str = cleaned_str.replace(",","")
+cleaned_str = cleaned_str.replace("/","")
+# cleaned_str = cleaned_str.replace("\","")
+lst = [word for word in cleaned_str.split(" ")]
+print(lst)
+
+str_mid = []
+print("Middle characters of words in above list are:")
+
+for string in lst:
+    if len(string) == 0:
+        pass
+    elif len(string) == 1:
+        str_mid.append(string)
+    elif len(string)%2 == 0:
+        mid = len(string)//2
+        str_mid.append(string[mid-1]+string[mid])
+    else:
+        mid = (len(string)//2)
+        str_mid.append(string[mid])
+print(str_mid)
--- userDefinedfunctions/Hard/multiple7_11.py
+++ userDefinedfunctions/Hard/multiple7_11.py
@@ -1,3 +1,11 @@
 '''check if a number is a multiple of 7 but not 11'''
+from functools import reduce
 
+def not7but11(nums):
+    '''returns the numbers in list which are divisible by 7 but not by 11'''
+    arr = filter(lambda x:(x%7==0)and(x%11!=0),nums)
+    return arr
 
+lst = [int(num) for num in input("Enter numbers seperated by comma").split(",")]
+print("Below numbers of list are divisible by 7 not by 11")
+print(list(not7but11(lst)))
