b9b9c236267f4dfb270cbaf271dba68c2b6d9308
Vaishnavi Gajinkar <vaishnavi.gajinkar@gmail.com>
2025-03-03 23:57:37 +0530

    string program 3Mar

--- userDefinedfunctions/Hard/startChar.py
+++ userDefinedfunctions/Hard/startChar.py
@@ -1,3 +1,24 @@
 '''check if a string starts with a specific character '''
 
+class begining:
+    def __init__ (self, word):
+        self.word = word
 
+    def strtWth(self,bigi):
+        self.bigi = bigi
+
+        if self.word.startswith(self.bigi):
+            return True
+        else:
+            return False
+        
+ask = input("Enter input word/string/number ")
+strt = input("Enter any character ")
+obj1 = begining(ask)
+
+result = obj1.strtWth(strt)
+
+if result:
+    print("Great. String starts with that char ")
+else:
+    print("incorrect")
