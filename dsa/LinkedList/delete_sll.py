8612a18ff7c977f7afd95939ae7f0338ce8a6242
Vaishnavi Gajinkar <vaishnavi.gajinkar@gmail.com>
2025-03-19 12:14:43 +0530

    deleting nodes from SLL 19Mar

--- dsa/LinkedList/delete_sll.py
+++ dsa/LinkedList/delete_sll.py
@@ -25,10 +25,24 @@ class Sll_addel:
         current.next = newnode
 
     def delete_node(self,value):
-        pass
+        current = self.head
+        print(f"\nNode with value {value} to be deleted...")
+
+        if current == None:
+            print("LL empty. Nothing to delete")
+            return
+        elif current.data == value:
+            self.head = self.head.next              # deleting head node
+        else:
+            while current is not None:
+                if current.data == value:
+                    break
+                tail = current
+                current= current.next               # deleting a non-head node
+            tail.next = current.next
 
     def display(self):
-
+        print("\nDisplaying the present state of list...")
         if self.head is None:
             print("LinkedList is empty")
             return
@@ -45,6 +59,38 @@ obj.add_node(10)
 obj.add_node(20)
 obj.add_node(30)
 obj.add_node(40)
+obj.add_node(50)
+obj.add_node(60)
+obj.add_node(70)
+obj.display()
+obj.delete_node(10)
+obj.display()
+obj.delete_node(40)
+obj.display()
+obj.delete_node(70)
 obj.display()
 
-# not solved
+# OUTPUT :
+"""
+Displaying the present state of list...
+LinkedList is empty
+
+Displaying the present state of list...
+10->20->30->40->50->60->70->None
+
+Node with value 10 to be deleted...
+
+Displaying the present state of list...
+20->30->40->50->60->70->None
+
+Node with value 40 to be deleted...
+
+Displaying the present state of list...
+20->30->50->60->70->None
+
+Node with value 70 to be deleted...
+
+Displaying the present state of list...
+20->30->50->60->None
+
+"""
