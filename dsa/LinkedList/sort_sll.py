67479b9ec2e5f1c884791898ed0e3595865290dd
Vaishnavi Gajinkar <vaishnavi.gajinkar@gmail.com>
2025-04-28 00:29:17 +0530

    Sorting LL

--- dsa/LinkedList/sort_sll.py
+++ dsa/LinkedList/sort_sll.py
@@ -13,28 +13,29 @@ def insAtEnd(head, data):
     if head == None:
         head = newnode
         return head
+    
     qtr = head
     ftr = qtr.next
-
     while ftr:
         qtr = qtr.next
         ftr = ftr.next
-    
     qtr.next = newnode
     return head
 
-def sortIt(head, data):
+def sortIt(head):
     ptr = head
-    arr = []
+    arr = []                                    # empty list to store all elements
 
     while ptr:
-        arr.append(ptr.data)
+        arr.append(ptr.data)                    # insert all elements of LL
         ptr = ptr.next
-    arr.sort()
+    arr.sort()                                  # sort the elements
+    # print(arr)
 
-    newptr = head
-    while
-
+    newptr = None                               # new pointer to create a LL of sorted elements
+    for n in range(len(arr)):
+        newptr = insAtEnd(newptr, arr[n])       # create nodes of the sorted list elements
+    return newptr
 
 def traverse(head):
     qtr = ftr = head
@@ -42,24 +43,35 @@ def traverse(head):
     while qtr:
         count += 1
         qtr = qtr.next
-    print(f"Currently there are {count} nodes in the LL")
+    print(f"Currently there are {count} nodes in the LL")       # displays count of nodes
 
     while ftr:
-        print(ftr.data, end="->")
+        print(ftr.data, end="->")                               # displays unsorted elements
         ftr = ftr.next
+    print("None\nAfter Sorting, elements of LL are rearranged as below")
+
+    srt = sortIt(head)                                          # implicit call to create LL of sorted elements
+    while srt:
+        print(srt.data, end="->")                               # displays sorted elements
+        srt = srt.next
     print("None")
 
 
+# a = Node(10)
+# b = Node(20)
+# c = Node(30)
+# d = Node(40)
 
-a = Node(10)
-b = Node(20)
-c = Node(30)
-d = Node(40)
+# a.next = b
+# b.next = c
+# c.next = d
+# d.next = None
 
-a.next = b
-b.next = c
-c.next = d
-d.next = None
+a = None
+a = insAtEnd(a,10)
+a = insAtEnd(a,20)
+a = insAtEnd(a,30)
+a = insAtEnd(a,40)
 
 a = insAtEnd(a,65)
 a = insAtEnd(a,32)
