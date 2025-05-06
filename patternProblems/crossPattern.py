849ccb20951e265948920572c4f1622ffb914a00
Vaishnavi Gajinkar <vaishnavi.gajinkar@gmail.com>
2025-05-06 23:56:51 +0530

    pattern probs

--- patternProblems/crossPattern.py
+++ patternProblems/crossPattern.py
@@ -5,3 +5,14 @@
  * * 
 *   * 
 '''
+rows = 5
+n = rows
+for i in range(rows):
+    for j in range(rows):
+        if i == j or (i-1)==j or i==(j-1):
+            print("*",end="")
+        else:
+            print(" ",end="")
+    print()
+
+# not solved        
