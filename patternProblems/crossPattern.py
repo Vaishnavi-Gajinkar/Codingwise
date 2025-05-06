a4dfe70931824731d8932b18a47951919ce03833
Vaishnavi Gajinkar <vaishnavi.gajinkar@gmail.com>
2025-05-07 00:06:58 +0530

    cross pattern

--- patternProblems/crossPattern.py
+++ patternProblems/crossPattern.py
@@ -5,14 +5,28 @@
  * * 
 *   * 
 '''
-rows = 5
+rows = int(input("Enter an odd number "))
 n = rows
 for i in range(rows):
     for j in range(rows):
-        if i == j or (i-1)==j or i==(j-1):
+        if i == j or (n-i-1)==j or i==(n-j-1):
             print("*",end="")
         else:
             print(" ",end="")
     print()
+if rows %2 == 0:
+    print("Told you to provide odd number na 😒")
 
-# not solved        
+'''
+OUTPUT :
+Enter an odd number 9
+*       *
+ *     *
+  *   *
+   * *
+    *
+   * *
+  *   *
+ *     *
+*       *
+'''
