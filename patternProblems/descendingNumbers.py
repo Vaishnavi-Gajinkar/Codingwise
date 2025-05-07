f313e246a0ff17a9032f3c486684e400d614f2a6
Vaishnavi Gajinkar <vaishnavi.gajinkar@gmail.com>
2025-05-07 23:30:33 +0530

    pattern probs

--- patternProblems/descendingNumbers.py
+++ patternProblems/descendingNumbers.py
@@ -5,3 +5,13 @@
 21
 1
 '''
+n = int(input("Enter number of rows "))
+decrmt = n
+
+for i in range(n):
+    decrmt = n - i
+    for j in range(n-i):
+        print(decrmt,end="")
+        decrmt -= 1
+    decrmt = n
+    print()
--- patternProblems/revAlphabetTriangle.py
+++ patternProblems/revAlphabetTriangle.py
@@ -5,3 +5,11 @@ ABC
 AB
 A
 '''
+n = int(input("Enter number of lines "))
+
+for i in range(n):
+    var = 65
+    for j in range(n-i):
+        print(chr(var),end="")
+        var += 1
+    print()
