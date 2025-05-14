96a732f10e7a462ae71326fd73f64e8286bfbbc3
Vaishnavi Gajinkar <vaishnavi.gajinkar@gmail.com>
2025-05-14 23:44:25 +0530

    DCLL deletions

--- dsa/LinkedList/circDoublyLL.py
+++ dsa/LinkedList/circDoublyLL.py
@@ -14,7 +14,7 @@ def insAtBigi(head, value):
         newnode = Node(value)
         newnode.next = newnode
         newnode.prev = newnode
-        print("Node added at begining of an empty DCLL")
+        print("Node added at begining of an empty CDLL")
         return newnode
     
     qtr = head
@@ -29,7 +29,7 @@ def insAtBigi(head, value):
     ftr.prev = newnode
     qtr.next = newnode
 
-    print("Node added at begining of a non-empty DCLL")
+    print("Node added at begining of a non-empty CDLL")
     return newnode
 
 def insAtEnd(head, value):
@@ -37,7 +37,7 @@ def insAtEnd(head, value):
         newnode = Node(value)
         newnode.next = newnode
         newnode.prev = newnode
-        print("Node added at end of an empty DCLL")
+        print("Node added at end of an empty CDLL")
         return newnode
     
     qtr = head
@@ -52,15 +52,15 @@ def insAtEnd(head, value):
     ftr.prev = newnode
     qtr.next = newnode
 
-    print("Node added at end of a non-empty DCLL")
+    print("Node added at end of a non-empty CDLL")
     return head
 
 def insAtBetween(head, pos, value):
     if head == None:
-        print("Node added at begining position of an empty DCLL")
+        print("Node added at begining position of an empty CDLL")
         return insAtBigi(head, value)
     if pos == 0:
-        print("Node added at begining position of an empty DCLL")
+        print("Node added at begining position of an empty CDLL")
         return insAtBigi(head, value)
     
     count = 1
@@ -83,19 +83,90 @@ def insAtBetween(head, pos, value):
         qtr.next = newnode
         ftr.prev = newnode
 
-        print(f"Node added at position {pos} of DCLL")
+        print(f"Node added at position {pos} of CDLL")
+        return head
+
+def delAtBigi(head):
+    if head == None:
+        print("CDLL is empty. Nothing to delete from begining")
+        return None
+    if head.next == head:
+        del head
+        print("Deleted the only node. CDLL is empty now")
+        return None
+    qtr = head
+    ftr = qtr.next
+    while ftr != head:
+        qtr = qtr.next
+        ftr = ftr.next
+
+    qtr.next = ftr.next
+    ftr.next.prev = qtr
+    del ftr
+    print("Node deleted from begining of CDLL")
+    return qtr.next
+
+def delFrmEnd(head):
+    if head == None:
+        print("CDLL empty. NOthing to delete from end")
+        return None
+    elif head.next == head:
+        del head
+        print("Deleted the only node from end. CDLL empty")
+        return None
+    
+    qtr = head
+    ftr = qtr.next
+    while ftr.next != head:
+        qtr = qtr.next
+        ftr = ftr.next
+    qtr.next = ftr.next
+    head.prev = qtr
+    del ftr
+    print("Deleted the node from end of CDLL")
+    return head
+
+def delFrmBtwn(head, pos):
+    if head == None:
+        print(f"CDLL empty. Nothing to delete at position {pos}")
+        return None
+    if pos == 0:
+        print(f"Deleted node from position {pos}")
+        return delAtBigi(head)
+    
+    count = 1
+    ptr = head.next
+    while ptr!=head:
+        count += 1
+        ptr = ptr.next
+    
+    if pos<count:
+        i = 0
+        qtr = head
+        ftr = qtr.next
+        while i < pos-1:
+            qtr = qtr.next
+            ftr = ftr.next
+            i += 1
+        qtr.next = ftr.next
+        ftr.next.prev = qtr
+        del ftr
+        print(f"Node deleted from position {pos} of CDLL")
+        return head
+    else:
+        print("Invalid index. Nothing to delete")
         return head
 
 def traverse(head):
     qtr = head.next
-    print("DCLL in proper order is:")
+    print("CDLL in proper order is:")
     print(head.data,end="<->")
     while qtr.next!=head:
         print(qtr.data,end="<->")
         qtr = qtr.next
     print(qtr.data)
 
-    print("DCLL in reverse order is:")
+    print("CDLL in reverse order is:")
     qtr = head.prev
     while qtr != head:
         print(qtr.data, end="<->")
@@ -124,18 +195,32 @@ a = insAtBigi(a, 5)
 a = insAtEnd(a, 50)
 a = insAtBetween(a, 3, 25)
 traverse(a)
+a = delAtBigi(a)
+a = delFrmEnd(a)
+a = delFrmBtwn(a, 3)
+traverse(a)
+
+
+
 
 '''
 OUTPUT :
-DCLL in proper order is:
+CDLL in proper order is:
 10<->20<->30<->40
-DCLL in reverse order is:
+CDLL in reverse order is:
 40<->30<->20<->10
-Node added at begining of a non-empty DCLL
-Node added at end of a non-empty DCLL
-Node added at position 3 of DCLL
-DCLL in proper order is:
+Node added at begining of a non-empty CDLL
+Node added at end of a non-empty CDLL
+Node added at position 3 of CDLL
+CDLL in proper order is:
 5<->10<->20<->25<->30<->40<->50
-DCLL in reverse order is:
+CDLL in reverse order is:
 50<->40<->30<->25<->20<->10<->5
+Node deleted from begining of CDLL
+Deleted the node from end of CDLL
+Node deleted from position 3 of CDLL
+CDLL in proper order is:
+10<->20<->25<->40
+CDLL in reverse order is:
+40<->25<->20<->10
 '''
