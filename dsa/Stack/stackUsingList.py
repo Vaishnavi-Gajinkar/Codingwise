6f64ef8e03966ae2fca75e0809caa6c6c6ddb0a2
Vaishnavi Gajinkar <vaishnavi.gajinkar@gmail.com>
2025-05-18 17:32:22 +0530

    stack using list

--- dsa/Stack/stackUsingList.py
+++ dsa/Stack/stackUsingList.py
@@ -1,6 +1,6 @@
 ''' Implement stack using a list '''
 
-class stk_lst:
+class stack:
     top = None
     arr = [None]
     size = None
@@ -10,9 +10,67 @@ class stk_lst:
         self.size = 10
         self.arr = self.arr * self.size
     
-def push(self):
-    if 
+def isEmpty(ptr):
+    if ptr == None or ptr.top == -1:
+        return True
+    else:
+        return False
 
+def isFull(ptr):
+    if ptr == None or ptr.top == (ptr.size)-1:
+        return True
+    else:
+        return False
+    
+def push(ptr, data):
+    if isFull(ptr):
+        print("Stack is Full. cannot add the element",data)
+        return ptr
+    else:
+        ptr.top += 1
+        ptr.arr[ptr.top] = data
+        return ptr
 
+def pop(ptr):
+    if isEmpty(ptr):
+        print("Stack is Empty. cannot pop anything")
+        return ptr
+    else:
+        val = ptr.arr[ptr.top] 
+        ptr.arr[ptr.top]=None
+        ptr.top -= 1
+        print(f"Element {val} removed from stack top")
+        return ptr
 
-head = stk_lst()
+ptr = stack()
+print(ptr.arr)
+pop(ptr)
+push(ptr,10)
+push(ptr,20)
+push(ptr,30)
+push(ptr,40)
+push(ptr,50)
+push(ptr,60)
+push(ptr,70)
+push(ptr,80)
+push(ptr,90)
+push(ptr,100)
+print(ptr.arr)
+push(ptr,110)
+pop(ptr)
+pop(ptr)
+print(ptr.arr)
+push(ptr, 111)
+print(ptr.arr)
+
+'''
+OUTPUT :
+[None, None, None, None, None, None, None, None, None, None]
+Stack is Empty. cannot pop anything
+[10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+Stack is Full. cannot add the element 110
+Element 100 removed from stack top
+Element 90 removed from stack top
+[10, 20, 30, 40, 50, 60, 70, 80, None, None]
+[10, 20, 30, 40, 50, 60, 70, 80, 111, None]
+'''
