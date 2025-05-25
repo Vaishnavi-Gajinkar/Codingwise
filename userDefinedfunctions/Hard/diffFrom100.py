88773b0728b0b34d2a5521f29cd91a968f4b77cc
Vaishnavi Gajinkar <vaishnavi.gajinkar@gmail.com>
2025-05-25 23:57:31 +0530

    random module choice func

--- userDefinedfunctions/Hard/diffFrom100.py
+++ userDefinedfunctions/Hard/diffFrom100.py
@@ -1,3 +1,24 @@
 '''calculate the difference between a number and 100, if the number is greater than 100'''
 
+import random
 
+def diffCalc(num):
+    if num < 100:
+        return 100-num
+    else:
+        return abs(num-100)
+    
+val = random.choice(range(1,200))
+res = diffCalc(val)
+print(f"Difference of number {val} from 100 is {res}")
+
+'''
+OUTPUT:
+Difference of number 112 from 100 is 12
+-------------------------------------------------------------------------------------------------
+Difference of number 62 from 100 is 38
+-------------------------------------------------------------------------------------------------
+Difference of number 181 from 100 is 81
+-------------------------------------------------------------------------------------------------
+Difference of number 20 from 100 is 80
+'''
--- userDefinedfunctions/Hard/parityCheck.py
+++ userDefinedfunctions/Hard/parityCheck.py
@@ -1,2 +1,37 @@
 '''check if two numbers are opposite in signs (pos/neg)'''
+import random
 
+def signCheck(num1,num2):
+    if num1 < 0 and num2 < 0:
+        print("Numbers have same signs")
+    elif num1 > 0 and num2 > 0:
+        print("Numbers have same signs")
+    elif num1 < 0 and num2 > 0:
+        print("Numbers differ in their sign")
+    elif num1 > 0 and num2 < 0:
+        print("Numbers differ in their sign")
+    else:
+        print("Eiter of the numbers are neutral. No sign to compare")
+
+# neg = float("inf")
+# pos = float("inf")
+
+val1 = random.choice(range(-500,500))
+val2 = random.choice(range(-500,500))
+
+res = signCheck(val1, val2)
+print(f"{val1} and {val2} were the randomly generated numbers ")
+
+'''
+OUTPUT:
+Numbers have same signs
+484 and 496 were the randomly generated numbers 
+--------------------------------------------------------------------------------------------
+Numbers differ in their sign
+-388 and 83 were the randomly generated numbers 
+--------------------------------------------------------------------------------------------
+Numbers have same signs
+-341 and -180 were the randomly generated numbers 
+--------------------------------------------------------------------------------------------
+
+'''
