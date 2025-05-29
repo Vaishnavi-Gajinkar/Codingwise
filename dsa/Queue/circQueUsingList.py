4c73d2bc98cd5f34f23ad0dae1f86e3fe4700633
Vaishnavi Gajinkar <vaishnavi.gajinkar@gmail.com>
2025-05-29 23:55:51 +0530

    circular queue using list

--- dsa/Queue/circQueUsingList.py
+++ dsa/Queue/circQueUsingList.py
@@ -13,15 +13,16 @@ def isEmpty(ptr):
         return False
 
 def isFull(ptr):
-    if ptr.rear+1 == ptr.front:
+    if (ptr.rear+1)%ptr.size == ptr.front:              # states that 1 cycle is completed
         return True
     else:
         return False
 
 def enqueue(head, value):
+    '''new values will be inserted/overwritten circularly until nothing is dequeued'''
     ptr = head
-    if isFull(ptr):
-        print("Queue is full now. Cannot enqueue")
+    if isFull(ptr):                                     
+        print("Queue is full now. Cannot enqueue")          # will be true when dequeue starts
         return ptr
     ptr.rear = (ptr.rear+1)%(ptr.size)
     ptr.arr[ptr.rear] = value
@@ -37,15 +38,15 @@ def dequeue(head):
     print("Element dequeued.")
     return head
 
-def peek(ptr):
-    print(ptr.arr[ptr.front+1],end="-")
-    start = ptr.front+2
-    end = ptr.rear
-    while start < end:
-        print(ptr.arr[start],end="-")
-        start = (start+1)%ptr.size
-    print(ptr.arr[ptr.rear])
-    return ptr
+# def peek(ptr):
+#     print(ptr.arr[ptr.front+1],end="-")
+#     start = ptr.front+2
+#     end = ptr.rear
+#     while start < end:
+#         print(ptr.arr[start],end="-")
+#         start = (start+1)%ptr.size
+#     print(ptr.arr[ptr.rear])
+#     return ptr
 
 def peekFront(ptr):
     print("Peek front",ptr.arr[ptr.front+1])
@@ -73,34 +74,37 @@ cq = enqueue(cq, 70)
 cq = enqueue(cq, 80)
 cq = enqueue(cq, 90)
 cq = enqueue(cq, 100)
+cq = enqueue(cq, 105)                                   # overwrites as dequeue operation is not performed yet
 
 print(cq.arr)
-peek(cq)
 
 cq = dequeue(cq)
 cq = dequeue(cq)
-cq = dequeue(cq)
-cq = dequeue(cq)
+
 print(cq.arr)
 cq = enqueue(cq, 110)
 cq = enqueue(cq, 120)
+cq = enqueue(cq, 130)
+cq = enqueue(cq, 140)
+cq = enqueue(cq, 150)
+cq = enqueue(cq, 160)
+cq = enqueue(cq, 170)
+cq = enqueue(cq, 180)
+cq = enqueue(cq, 190)
+cq = enqueue(cq, 200)                                       # since front is moved and tried to rewrite, hence q Full error is displayed
 print(cq.arr)
-peek(cq)
 peekFront(cq)
 peekRear(cq)
 
 '''
 OUTPUT:
 Circular queue is empty. Nothing to dequeue
-[10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
-10-20-30-40-50-60-70-80-90-100
+[105, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 Element dequeued.
-Element dequeued.
-Element dequeued.
-Element dequeued.
-[None, None, None, None, 50, 60, 70, 80, 90, 100]
-[110, 120, None, None, 50, 60, 70, 80, 90, 100]
-50-120
-Peek front 50
-Peek rear  120
+Circular queue is empty. Nothing to dequeue
+[None, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+Queue is full now. Cannot enqueue
+[None, 110, 120, 130, 140, 150, 160, 170, 180, 190]
+Peek front 110
+Peek rear  190
 '''
